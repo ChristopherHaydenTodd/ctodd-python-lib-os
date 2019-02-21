27e554c4077bd2e59713710685f94e5ce7eec30c
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-21 16:26:27 -0500

    adding  Library for interacting with files in an environment.

---
+++ os_helpers/file_helpers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Helper Library for interacting with files in an environment.
+"""
+
+# Python Library Imports
+import os
+import sys
+import logging
+
+
+###
+# File Helpers
+###
